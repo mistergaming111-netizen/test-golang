78ee554588128413be6cef61c5ba6b62cb402cf4
mistergaming111-netizen <mistergaming111@gmail.com>
2026-09-07 18:19:07 +0200

    Mise à jour du jeu

--- main.go
+++ main.go
@@ -3,6 +3,21 @@ package main
 import "fmt"
 
 func main() {
-    letter := 'é'
-    fmt.Printf(" %c\n", letter)
+    question := "Devine un nombre entre 1 et 10"
+    fmt.Println(question)
+    nombreSecret := 7
+    fmt.Println("Entre ton nombre :")
+    var choix int
+    fmt.Scan(&choix)
+    for choix < 1 || choix > 10 {
+        fmt.Println("Le nombre doit être entre 1 et 10. Entre un nouveau nombre :")
+        fmt.Scan(&choix)
+    }
+    if choix == nombreSecret {
+        fmt.Println("Bravo ! Tu as deviné le nombre secret.")
+    }else if choix < nombreSecret {
+        fmt.Println("Le nombre secret est plus grand que ton choix.")
+    } else {
+        fmt.Println("Le nombre secret est plus petit que ton choix.")
+    }
 }
